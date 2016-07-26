b8eb449e9fa47d52171b02e1da2779918447da57
Topher-Tree <chris.ogletree@verizon.net>
2016-07-26 15:37:28 -0500

    jsonp

--- weather-app-script.js
+++ weather-app-script.js
@@ -31,7 +31,7 @@ function dispTemp() {
 	
 	currentDate = startDate;
 	
-	weatherURL = "https://api.weathersource.com/v1/6bde010b79d7f87f0397/history_by_postal_code.json?_callback=dispTemp?period=day&postal_code_eq=" + zipCode + "&country_eq=US&timestamp_eq=" + currentDate + "&fields=tempAvg";
+	weatherURL = "https://api.weathersource.com/v1/6bde010b79d7f87f0397/history_by_postal_code.json?_callback=parseWeather?period=day&postal_code_eq=" + zipCode + "&country_eq=US&timestamp_eq=" + currentDate + "&fields=tempAvg";
 	
 	for(currentDate.getDate(); currentDate.getDate() <= endDate.getDate(); currentDate.setDate(currentDate.getDate() + 1)) {
 		
