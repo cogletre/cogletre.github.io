83254d5cec1fcd97e02ae92a207825df9011d3cc
Topher-Tree <chris.ogletree@verizon.net>
2016-07-28 13:44:18 -0500

    accordion "active"

--- portfolio-script.js
+++ portfolio-script.js
@@ -3,13 +3,13 @@
 $(document).ready(function(){
     
 	$( "#work-accordion" ).accordion({
-		active:-1,
+		active:false,
 		collapsible:true,
 		icons: { "header": "ui-icon-plus", "activeHeader": "ui-icon-minus" }
 	});
 	
 	$( "#projects-accordion" ).accordion({
-		active:-1,
+		active:false,
 		collapsible:true,
 		icons: { "header": "ui-icon-plus", "activeHeader": "ui-icon-minus" }
 	});
