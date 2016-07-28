f877c4abcfe76ed1e017c0742935b5ae43d4269a
Topher-Tree <chris.ogletree@verizon.net>
2016-07-28 13:03:36 -0500

    More portfolio work

---
+++ portfolio-script.js
@@ -0,0 +1,14 @@
+
+
+$(document).ready(function(){
+    
+	$( "#work-accordion" ).accordion({
+		active:0,
+		collapsible:true
+	});
+	
+	$( "#projects-accordion" ).accordion({
+		active:0,
+		collapsible:true
+	});
+});
