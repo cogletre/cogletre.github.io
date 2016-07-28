e4f314cc23f9c80d24e994b562b0df8cd094343c
Topher-Tree <chris.ogletree@verizon.net>
2016-07-28 13:31:00 -0500

    More style plus accordion work

--- portfolio-script.js
+++ portfolio-script.js
@@ -3,12 +3,14 @@
 $(document).ready(function(){
     
 	$( "#work-accordion" ).accordion({
-		active:0,
-		collapsible:true
+		active:-1,
+		collapsible:true,
+		icons: { "header": "ui-icon-plus", "activeHeader": "ui-icon-minus" }
 	});
 	
 	$( "#projects-accordion" ).accordion({
-		active:0,
-		collapsible:true
+		active:-1,
+		collapsible:true,
+		icons: { "header": "ui-icon-plus", "activeHeader": "ui-icon-minus" }
 	});
 });
