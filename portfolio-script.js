a310e056ef1f658bffa0e074442109c8756c9569
Topher-Tree <chris.ogletree@verizon.net>
2016-07-28 13:50:19 -0500

    accordion heights

--- portfolio-script.js
+++ portfolio-script.js
@@ -5,12 +5,14 @@ $(document).ready(function(){
 	$( "#work-accordion" ).accordion({
 		active:false,
 		collapsible:true,
-		icons: { "header": "ui-icon-plus", "activeHeader": "ui-icon-minus" }
+		icons: { "header": "ui-icon-plus", "activeHeader": "ui-icon-minus" },
+		heightStyle: "content"
 	});
 	
 	$( "#projects-accordion" ).accordion({
 		active:false,
 		collapsible:true,
-		icons: { "header": "ui-icon-plus", "activeHeader": "ui-icon-minus" }
+		icons: { "header": "ui-icon-plus", "activeHeader": "ui-icon-minus" },
+		heightStyle: "content"
 	});
 });
