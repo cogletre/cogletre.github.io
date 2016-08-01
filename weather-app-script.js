598ae9124e964b19480f712918837251466caa6f
Topher-Tree <chris.ogletree@verizon.net>
2016-08-01 14:48:52 -0500

    recommit js

--- weather-app-script.js
+++ weather-app-script.js
@@ -1,4 +1,3 @@
-
 var zipCode;
 var startDate,endDate,currentDate;
 
@@ -6,59 +5,39 @@ var currDayTemp;
 var currTempArray = [];
 
 var weatherURL;
-
-//var totalTemp = 0;
-var totalDays = 0;
-
-var avgTemp;
+var weatherXmlHttp = new XMLHttpRequest();
 
 var tempScript = null;
 
-var dateError = "Please choose a valid date range";
-var nullError = "Please fill in every field";
+var totalTemp = 0;
+var totalDays = 0;
+
+var avgTemp;
 
 
 //Callback function for JSONP request to WeatherSource.com API
 function readWeather(weatherData) {
 	
+	if(tempScript){
+		document.getElementsByTagName('head')[0].removeChild(tempScript);
+		tempScript = null;
+	}
+
 	var tempObj = weatherData[0];
 
 	currDayTemp = parseInt(tempObj.tempAvg);
-	
-	currTempArray[currTempArray.length] = currDayTemp;
-	totalDays = totalDays + 1;
-	
-	//totalTemp += currDayTemp;
-	//totalDays++;
-	
-	//avgTemp = parseInt(totalTemp/totalDays);
-	
-	//document.getElementById("avg-temp-output").innerHTML = avgTemp + "&deg;";
-	
-	/*if (currentDate.getDate() !< endDate.getDate()) {
-		var totalTemp = 0,totalDays = 0;
 		
-		for(var tem in currTempArray){
-			totalTemp += currTempArray[tem];
-			totalDays++;
-		}
-		
-		var avgTemp = totalTemp / totalDays;
-		
-		document.getElementById("avg-temp-output").innerHTML = avgTemp + "&deg;";
-		document.getElementById("city-zip").innerHTML = zipCode;
-	}
+	totalTemp += currDayTemp;
+	totalDays++;
 	
-	if(tempScript) {
-		document.getElementsByTagName('head')[0].removeChild(tempScript);
-	}*/
+	avgTemp = parseInt(totalTemp/totalDays);
+	
+	document.getElementById("avg-temp-output").innerHTML = avgTemp + "&deg;";
 }
 
 
 
 function dispTemp() {
-	
-	var totalTemp = 0;
 	
 	zipCode = document.getElementById("zipCode").value;
 	
@@ -67,23 +46,9 @@ function dispTemp() {
 	
 	currentDate = startDate;
 	
-	/*var lowestDate = new Date('December 31, 1979 11:59:99');
-	
-	if(zipCode === null || startDate === null || endDate === null) {
-		document.getElementById("dateErrorField").innerHTML = nullError;
 		
-		document.getElementById("dateErrorField").removeAttribute("tempHidden");
-	}
-	if(startDate.getDate() <= lowestDate || startDate.getDate() > endDate.getDate()) {
-		document.getElementById("dateErrorField").innerHTML = dateError;
-		
-		document.getElementById("dateErrorField").removeAttribute("tempHidden");
-	} else {*/
-		
-	for(startDate.getDate(); currentDate.getDate() <= endDate.getDate(); currentDate.setDate(currentDate.getDate() + 1)){
+	while(currentDate.getDate() <= endDate.getDate()){
 		currDateStr = currentDate.toDateString();
-		
-		currentDate.setDate(currentDate.getDate() + 1);
 		
 		weatherURL = "https://api.weathersource.com/v1/6bde010b79d7f87f0397/history_by_postal_code.json?_callback=readWeather&period=day&postal_code_eq="+zipCode+"&country_eq=US&timestamp_eq="+currDateStr+"T12:00+00:00&fields=tempAvg";
 		
@@ -92,20 +57,17 @@ function dispTemp() {
 		
 		document.getElementsByTagName('head')[0].appendChild(tempScript);
 		
-		console.log("did it one time");
-	}
-	//}
-	
-	for(var tem in currTempArray){
-		totalTemp += currTempArray[tem];
-		totalDays++;
+		currentDate.setDate(currentDate.getDate() + 1);
 	}
 	
-	var avgTemp = totalTemp / totalDays;
-		
-	document.getElementById("avg-temp-output").innerHTML = avgTemp + "&deg;";
 	document.getElementById("city-zip").innerHTML = zipCode;
+	
+	zipCode = null;
+	avgTemp = 0;
+	totalDays = 0;
+	totalTemp = 0;
+	currDayTemp = 0;
+	startDate = null;
+	endDate = null;
+	currentDate = null;
 }
-
-
-
