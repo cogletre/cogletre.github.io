4eaa2dd0ed05e8cae405200310d09c2711b793c0
Topher-Tree <chris.ogletree@verizon.net>
2016-08-01 13:57:22 -0500

    more changes

--- weather-app-script.js
+++ weather-app-script.js
@@ -21,7 +21,7 @@ var nullError = "Please fill in every field";
 //Callback function for JSONP request to WeatherSource.com API
 function readWeather(weatherData) {
 	
-	console.log(weatherData[0].tempAvg); 
+	console.log(weatherData); 
 	
 	/*if(tempScript) {
 		document.getElementsByTagName('head')[0].removeChild(tempScript);
