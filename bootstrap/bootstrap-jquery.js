5a568eafdefc16b3246e5598ace5284aaf8340ee
Topher-Tree <chris.ogletree@verizon.net>
2016-08-08 13:02:50 -0500

    jquery edits

--- bootstrap/bootstrap-jquery.js
+++ bootstrap/bootstrap-jquery.js
@@ -21,9 +21,9 @@ $( document ).ready(function(){
 		heightStyle: "content"
 	});
 	
-	if( $(window).width() < 768px) {
+	if( $(window).width() < 768) {
 		$("div.border").removeClass("col-xs-1");
-		$("div.border").attr("min-width",2%);
+		$("div.border").attr("min-width","2%");
 		$("div.main-content").removeClass("col-xs-10");
 		$("div.main-content").addClass("col-xs-12");
 	} else {
