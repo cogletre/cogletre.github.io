66a815a5cd10c9fda8508c122867fadbbd5b1b59
Topher-Tree <chris.ogletree@verizon.net>
2016-08-08 12:54:25 -0500

    query classes

--- bootstrap/bootstrap-jquery.js
+++ bootstrap/bootstrap-jquery.js
@@ -15,11 +15,15 @@ $( document ).ready(function(){
 	});
 	
 	if(window.width() < 768px) {
-		$("div.border").removeClass("col-xs-2");
+		$("div.border").removeClass("col-xs-1");
 		$("div.border").attr("min-width",2%);
+		$("div.main-content").removeClass("col-xs-10");
+		$("div.main-content").addClass("col-xs-12");
 	} else {
-		$("div.border").addClass("col-xs-2");
+		$("div.border").addClass("col-xs-1");
 		$("div.border").removeAttr("min-width");
+		$("div.main-content").addClass("col-xs-10");
+		$("div.main-content").removeClass("col-xs-12");
 	}
 });
 
