9413934cb9572986211cc0f76412fa1af3124f58
Topher-Tree <chris.ogletree@verizon.net>
2016-08-08 13:43:55 -0500

    classes jquery

--- bootstrap/bootstrap-jquery.js
+++ bootstrap/bootstrap-jquery.js
@@ -47,9 +47,11 @@ $( window ).resize( function(){
 		$("div.main-content").addClass("col-xs-12");
 	} else if( $(window).width() >= 544 &&  $(window).width() < 992) {
 		$("div.border").addClass("col-xs-1");
+		$("div.border").removeClass("col-xs-2");
 		$("div.border").removeAttr("min-width");
 		$("div.main-content").addClass("col-xs-10");
 		$("div.main-content").removeClass("col-xs-12");
+		$("div.main-content").removeClass("col-xs-8");
 	} else {
 		$("div.border").addClass("col-xs-2");
 		$("div.border").removeClass("col-xs-1");
