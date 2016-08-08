a7501a512da25e270d99b92d024634a0e2e6c70c
Topher-Tree <chris.ogletree@verizon.net>
2016-08-08 13:40:07 -0500

    resize functions

--- bootstrap/bootstrap-jquery.js
+++ bootstrap/bootstrap-jquery.js
@@ -1,29 +1,23 @@
-$( document ).ready(function(){
+$( document ).ready( function(){
     
 	$("div.border").height( $("div.main-content").height() );
 	
-	$( window ).resize( function(){
-		
-		$( "div.border" ).height( $("div.main-content").height() );
-		
-		
-		if( $(window).width() < 544 ) {
-			$("div.border").removeClass("col-xs-1");
-			$("div.border").attr("max-width","1%");
-			$("div.main-content").removeClass("col-xs-10");
-			$("div.main-content").addClass("col-xs-12");
-		} else if( $(window).width() >= 544 &&  $(window).width() < 992) {
-			$("div.border").addClass("col-xs-1");
-			$("div.border").removeAttr("min-width");
-			$("div.main-content").addClass("col-xs-10");
-			$("div.main-content").removeClass("col-xs-12");
-		} else {
-			$("div.border").addClass("col-xs-2");
-			$("div.border").removeClass("col-xs-1");
-			$("div.main-content").removeClass("col-xs-10");
-			$("div.main-content").addClass("col-xs-8");
-		}
-	});
+	if( $(window).width() < 544 ) {
+		$("div.border").removeClass("col-xs-1");
+		$("div.border").attr("max-width","1%");
+		$("div.main-content").removeClass("col-xs-10");
+		$("div.main-content").addClass("col-xs-12");
+	} else if( $(window).width() >= 544 &&  $(window).width() < 992) {
+		$("div.border").addClass("col-xs-1");
+		$("div.border").removeAttr("min-width");
+		$("div.main-content").addClass("col-xs-10");
+		$("div.main-content").removeClass("col-xs-12");
+	} else {
+		$("div.border").addClass("col-xs-2");
+		$("div.border").removeClass("col-xs-1");
+		$("div.main-content").removeClass("col-xs-10");
+		$("div.main-content").addClass("col-xs-8");
+	}
 	
 	$( "#work-accordion" ).accordion({
 		active:false,
@@ -42,3 +36,24 @@ $( document ).ready(function(){
 	
 });
 
+$( window ).resize( function(){
+		
+	$( "div.border" ).height( $("div.main-content").height() );
+	
+	if( $(window).width() < 544 ) {
+		$("div.border").removeClass("col-xs-1");
+		$("div.border").attr("max-width","1%");
+		$("div.main-content").removeClass("col-xs-10");
+		$("div.main-content").addClass("col-xs-12");
+	} else if( $(window).width() >= 544 &&  $(window).width() < 992) {
+		$("div.border").addClass("col-xs-1");
+		$("div.border").removeAttr("min-width");
+		$("div.main-content").addClass("col-xs-10");
+		$("div.main-content").removeClass("col-xs-12");
+	} else {
+		$("div.border").addClass("col-xs-2");
+		$("div.border").removeClass("col-xs-1");
+		$("div.main-content").removeClass("col-xs-10");
+		$("div.main-content").addClass("col-xs-8");
+	}
+});
