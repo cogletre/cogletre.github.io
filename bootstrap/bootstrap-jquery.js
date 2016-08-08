e677e02f610b06eac94fc7c5174347ecb428385a
Topher-Tree <chris.ogletree@verizon.net>
2016-08-08 13:33:08 -0500

    resizing

--- bootstrap/bootstrap-jquery.js
+++ bootstrap/bootstrap-jquery.js
@@ -4,6 +4,23 @@ $( document ).ready(function(){
 	
 	$( window ).resize( function(){
 		$( "div.border" ).height( $("div.main-content").height() );
+		
+		if( $(window).width() < 544 ) {
+			$("div.border").removeClass("col-xs-1");
+			$("div.border").attr("max-width","1%");
+			$("div.main-content").removeClass("col-xs-10");
+			$("div.main-content").addClass("col-xs-12");
+		} else if( $(window).width() >= 544 &&  $(window).width() < 992) {
+			$("div.border").addClass("col-xs-1");
+			$("div.border").removeAttr("min-width");
+			$("div.main-content").addClass("col-xs-10");
+			$("div.main-content").removeClass("col-xs-12");
+		} else {
+			$("div.border").addClass("col-xs-2");
+			$("div.border").removeClass("col-xs-1");
+			$("div.main-content").removeClass("col-xs-10");
+			$("div.main-content").addClass("col-xs-8");
+		}
 	});
 	
 	$( "#work-accordion" ).accordion({
@@ -20,21 +37,6 @@ $( document ).ready(function(){
 		heightStyle: "content"
 	});
 	
-	if( $(window).width() < 544 ) {
-		$("div.border").removeClass("col-xs-1");
-		$("div.border").attr("max-width","1%");
-		$("div.main-content").removeClass("col-xs-10");
-		$("div.main-content").addClass("col-xs-12");
-	} else if( $(window).width() >= 544 &&  $(window).width() < 992) {
-		$("div.border").addClass("col-xs-1");
-		$("div.border").removeAttr("min-width");
-		$("div.main-content").addClass("col-xs-10");
-		$("div.main-content").removeClass("col-xs-12");
-	} else {
-		$("div.border").addClass("col-xs-2");
-		$("div.border").removeClass("col-xs-1");
-		$("div.main-content").removeClass("col-xs-10");
-		$("div.main-content").addClass("col-xs-8");
-	}
+	
 });
 
