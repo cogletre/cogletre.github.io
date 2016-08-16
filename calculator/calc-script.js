9a5a0cdba1e7de2e4b1db810514c9ca9bfd9166a
Topher-Tree <chris.ogletree@verizon.net>
2016-08-16 15:52:08 -0400

    add some jquery

---
+++ calculator/calc-script.js
@@ -0,0 +1,27 @@
+
+$( document ).ready( function() {
+	
+	var finEquation = "";
+	
+	var finVal;
+	
+	function numClick( input ) {
+		
+		finEquation.pop( input );
+		
+		$( "#display-box" ).innerHTML(finEquation);
+	}
+	
+	function clearEquation() {
+		
+		finEquation = "";
+		
+		$( "#display-box" ).innerHTML( "0" );
+	}
+	
+	function equationEquals() {
+		finVal = eval( finEquation );
+		
+		$( "#display-box" ).innerHTML( finVal );
+	}
+});
