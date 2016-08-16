b06bc495c741afb7be6d47b41f87e738f0647551
Topher-Tree <chris.ogletree@verizon.net>
2016-08-16 16:00:11 -0400

    js

--- calculator/calc-script.js
+++ calculator/calc-script.js
@@ -10,18 +10,18 @@ $( document ).ready( function() {
 		finEquation.pop( input );
 		
 		$( "#display-box" ).innerHTML(finEquation);
-	}
+	};
 	
 	function clearEquation() {
 		
 		finEquation = "";
 		
 		$( "#display-box" ).innerHTML( "0" );
-	}
+	};
 	
 	function equationEquals() {
 		finVal = eval( finEquation );
 		
 		$( "#display-box" ).innerHTML( finVal );
-	}
+	};
 });
