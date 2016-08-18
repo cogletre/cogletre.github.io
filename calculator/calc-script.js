4daa961613efbdd4e73aced7902eb4c7b18b07b6
Topher-Tree <chris.ogletree@verizon.net>
2016-08-18 17:03:31 -0400

    might actually work this time

--- calculator/calc-script.js
+++ calculator/calc-script.js
@@ -1,31 +1,26 @@
+var finEquation = "";
+var finVal;
+
 
 $( document ).ready( function() {
-	
-	var finEquation = "";
-	
-	var finVal;
-	
-	
-	
-	
 });
 
 function numClick( input ) {
-		
-		finEquation.pop( input.toString() );
-		
-		$( "#display-box" ).innerHTML(finEquation);
-	};
 	
-	function clearEquation() {
-		
-		finEquation = "";
-		
-		$( "#display-box" ).innerHTML( "0" );
-	};
+	finEquation.pop( input.toString() );
 	
-	function equationEquals() {
-		finVal = eval( finEquation );
-		
-		$( "#display-box" ).innerHTML( finVal );
-	};
+	$( "#display-box" ).innerHTML(finEquation);
+};
+
+function clearEquation() {
+	
+	finEquation = "";
+	
+	$( "#display-box" ).innerHTML( "0" );
+};
+
+function equationEquals() {
+	finVal = eval( finEquation );
+	
+	$( "#display-box" ).innerHTML( finVal );
+};
