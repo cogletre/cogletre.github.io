2ed42b434eb83635af572641ebdce404ab4e9b09
Topher-Tree <chris.ogletree@verizon.net>
2016-08-18 17:22:13 -0400

    innerHTML

--- calculator/calc-script.js
+++ calculator/calc-script.js
@@ -3,27 +3,28 @@ var finVal;
 
 
 $( document ).ready( function() {
-	$( "#display-equation" ).innerHTML = "0";
+	
+	document.getElementById("display-box").innerHTML = finEquation;
 });
 
 function numClick( input ) {
 	
 	finEquation = finEquation + input.toString();
 	
-	$( "#display-equation" ).innerHTML = finEquation;
+	document.getElementById("display-box").innerHTML = finEquation;
 };
 
 function clearEquation() {
 	
 	finEquation = "0";
 	
-	$( "#display-equation" ).innerHTML = finEquation;
+	document.getElementById("display-box").innerHTML = finEquation;
 };
 
 function equationEquals() {
 	finVal = eval( finEquation );
 	
-	$( "#display-equation" ).innerHTML = finVal;
+	document.getElementById("display-box").innerHTML = finVal;
 	
 	finEquation = "0";
 };
