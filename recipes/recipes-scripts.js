c39683a5e92c41798434ccf7f5da14b43fb92c4b
Chris Ogletree <cogletre@gmail.com>
2020-12-30 13:37:55 -0500

    recipe JSON

--- recipes/recipes-scripts.js
+++ recipes/recipes-scripts.js
@@ -4,7 +4,7 @@
 
 function loadRecipes(callback) {
 	var xhttp=new XMLHttpRequest();
-	xhttp.overrideMimeType('application/json');
+	xhttp.overrideMimeType("application/json");
 	xhttp.open('GET','https://cogletre.github.io/recipes/recipe-structure.json',true);
 	xhttp.onreadystatechange=function() {
 		if(this.readyState==4 && this.status==200) {
